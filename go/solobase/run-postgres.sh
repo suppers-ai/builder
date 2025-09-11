11f6125e3ead2f57e1105c9f66a521bdf2fb2e39
Jsuppers <jorissuppers@gmail.com>
2025-09-12 11:41:39 +1200

    refactor remove old files

--- go/solobase/run-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DATABASE_TYPE=postgres
-export DATABASE_URL="postgresql://postgres:postgres@127.0.0.1:54322/postgres?sslmode=disable"
-export DEFAULT_ADMIN_EMAIL="admin@example.com"  
-export DEFAULT_ADMIN_PASSWORD="admin123"
-export PORT=8091
-./solobase
